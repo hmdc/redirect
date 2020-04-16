4cf7cb0012dd995c551d702b6b993caabcc490a4
Evan Sarmiento <esarmien@g.harvard.edu>
2020-04-16 11:57:46 -0400

    Gis shold be primary subdomain.

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     "defaultRedirect": "https://www.iq.harvard.edu",
     "port": process.env.PORT || 3000,
     "redirectMap": [
-        { "src": "^(.*\\.)cga\\.harvard\\.edu$", "dst": "https://www.gis.harvard.edu" },
+        { "src": "^(.*\\.)cga\\.harvard\\.edu$", "dst": "https://gis.harvard.edu" },
         { "src": "^(.*\\.)africamap\\.harvard\\.edu$", "dst": "https://worldmap.harvard.edu/africamap" },
         { "src": "^(.*\\.)?chgis\\.hmdc\\.harvard\\.edu$", "dst": "https://maps.cga.harvard.edu" },
         { "src": "^(.*\\.)?gking-projects\\.hmdc\\.harvard\\.edu$", "dst": "https://gking-projects.iq.harvard.edu" },
